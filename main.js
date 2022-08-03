c796a4139a151beb973a1b0e3ded84cd67ec2557
falcons2013 <trilobyte88@gmail.com>
2022-08-03 16:53:22 -0700

    added animations and topdown movement

--- main.js
+++ main.js
@@ -1,36 +1,76 @@
+
+let cursors;
+let player;
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super();
+    }
+    preload() {
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.load.spritesheet('dude', "assets/dogan.png", { frameWidth: 32, frameHeight: 32 });
+    }
+    create() {
+        player = this.physics.add.sprite(100, 450, 'player');
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 2, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
+        player.width = 32;
+        player.height = 32;
+        console.log(this.anims.get("idle"));
+        console.log(cursors);
+    }
+    update() {
+        const MAX_VEL = 100;
+        if (cursors.up.isDown) {
+            player.setVelocityY(-MAX_VEL);
+            //player.anims.play("up"); 
+        } else if (cursors.down.isDown) {
+            player.setVelocityY(MAX_VEL);
+            //player.anims.play("up"); 
+        } else if (cursors.left.isDown) {
+            player.anims.play("left"); 
+            player.setVelocityX(-MAX_VEL);
+        } else if (cursors.right.isDown) {
+            player.anims.play("right"); 
+            player.setVelocityX(MAX_VEL);
+        } else {
+            player.setVelocityX(0);
+            player.setVelocityY(0);
+        }
+    }
+}
+
 let config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+    width: 512,
+    height: 512,
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+
+    },
+    scene: GameScene,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true,
+        }
     }
 };
 
 
 let game = new Phaser.Game(config);
-
-let cursors;
-class GameScene extends Phaser.Scene {
-    constructor() {
-    }
-    preload() {
-        cursors = this.input.keyboard.createCursorKeys();
-    }
-    create() {
-        console.log(cursors);
-    }
-    update() {
-        let test = this.GameObjects.Shape(this,);
-        test.x = 0.0;
-        test.y = 0.0;
-    }
-}
-
-
-
-function update() {
-
-}
