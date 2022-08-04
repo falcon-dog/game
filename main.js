3185774974aae7734f5c3c06d0c094388b002ab6
falcons2013 <trilobyte88@gmail.com>
2022-08-03 19:24:02 -0700

    ADDED: moving player, enemies, health and death

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+'use strict';
+const WIDTH = 512;
+const HEIGHT = 512;
 
+const BG_LAYER = 0; //background, tiles
+const MOB_LAYER = 1; //players
+const HUD_LAYER = 3;
+
+/** @type {Phaser.Types.Curves} */
 let cursors;
-let player;
+/** @type {Phaser.Types.Physics.Arcade.SpriteWithDynamicBody} */
+let player; //todo: use class to construct instead
+let player_health = 400;
+
+/** @type {Phaser.Types.Physics.Arcade.SpriteWithDynamicBody[]} */
+let enemies = [];
+let space_key;
+
+/**  @type {Phaser.GameObjects. Text } */
+let health_hud;
+
 class GameScene extends Phaser.Scene {
     constructor() {
         super();
@@ -8,66 +26,144 @@ class GameScene extends Phaser.Scene {
     preload() {
         cursors = this.input.keyboard.createCursorKeys();
 
-        this.load.spritesheet('dude', "assets/dogan.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.image('sky', 'assets/sky.png');
+        this.load.spritesheet('dog', "assets/dogan.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('sqrl', "assets/sqrl.png", { frameWidth: 32, frameHeight: 32 });
     }
     create() {
-        player = this.physics.add.sprite(100, 450, 'player');
+        health_hud = this.add.text(0, 0, `HP: ${player_health}`);
+        health_hud.setDepth(HUD_LAYER);
 
+        let img = this.add.image(0, 0, 'sky');
+        img.setDepth(0);
+
+        player = this.physics.add.sprite(100, 450, 'player');
+        player.setDepth(MOB_LAYER);
+
+        space_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        // create animations
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 1 }),
+            frames: this.anims.generateFrameNumbers('dog', { start: 0, end: 1 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 2, end: 3 }),
+            frames: this.anims.generateFrameNumbers('dog', { start: 2, end: 3 }),
             frameRate: 10,
             repeat: -1
         });
 
+        this.anims.create({
+            key: 'left_bark',
+            frames: this.anims.generateFrameNumbers('dog', { start: 6, end: 10 }),
+            frameRate: 10,
+        });
 
+        this.anims.create({
+            key: 'right_bark',
+            frames: this.anims.generateFrameNumbers('dog', { start: 11, end: 15 }),
+            frameRate: 10,
+        });
+
+        this.anims.create({
+            key: 'death',
+            frames: this.anims.generateFrameNumbers('dog', { start: 16, end: 18 }),
+            frameRate: 10,
+        });
+
+        this.anims.create({
+            key: 'sqrl_left',
+            frames: this.anims.generateFrameNumbers('sqrl', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'sqrl_right',
+            frames: this.anims.generateFrameNumbers('sqrl', { start: 2, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.cameras.main.setSize(WIDTH, HEIGHT);
+        this.cameras.main.startFollow(player);
+
+        player.anims.play("left");
         player.width = 32;
         player.height = 32;
-        console.log(this.anims.get("idle"));
-        console.log(cursors);
+
+        for (let i = 0; i < 4; i++) {
+            let enemy = this.physics.add.sprite(Phaser.Math.Between(0, WIDTH), Phaser.Math.Between(0, HEIGHT));
+            enemy.anims.play("sqrl_right");
+            //console.log(enemy);
+            enemy.setDepth(MOB_LAYER);
+            enemies.push(enemy);
+        }
     }
     update() {
         const MAX_VEL = 100;
+        health_hud.setText(`HP: ${player_health}`);
+        health_hud.x = player.x;
+        health_hud.y = player.y;
+        //health_hud.updateText();
+
+        if (player_health <= 0) {
+            player.anims.play("death", true);
+        }
         if (cursors.up.isDown) {
             player.setVelocityY(-MAX_VEL);
-            //player.anims.play("up"); 
         } else if (cursors.down.isDown) {
             player.setVelocityY(MAX_VEL);
-            //player.anims.play("up"); 
         } else if (cursors.left.isDown) {
-            player.anims.play("left"); 
+            player.anims.play("left");
             player.setVelocityX(-MAX_VEL);
         } else if (cursors.right.isDown) {
-            player.anims.play("right"); 
+            player.anims.play("right");
             player.setVelocityX(MAX_VEL);
         } else {
             player.setVelocityX(0);
             player.setVelocityY(0);
         }
+
+        if (space_key.isDown) {
+            //moving right, so bark right
+            player.anims.play("right_bark", true);
+        }
+
+        // per frame? should be something else
+        for (let sqrl of enemies) {
+            // get direction vector towards the player
+            let ran_x_vel = Phaser.Math.Between(20, 100);
+            let ran_y_vel = Phaser.Math.Between(20, 100);
+
+            this.physics.accelerateToObject(sqrl, player, 50, 80, 80);
+            this.physics.overlap(player, sqrl, (pl, en) => {
+                player_health -= 1;
+            })
+            //sqrl.setVelocityX(ran_x_vel);
+            //sqrl.setVelocityY(ran_y_vel);
+        }
+        // update enemy ai
     }
 }
 
 let config = {
     type: Phaser.AUTO,
-    width: 512,
-    height: 512,
+    width: WIDTH,
+    height: HEIGHT,
     pixelArt: true,
     scale: {
         mode: Phaser.Scale.FIT,
-
     },
     scene: GameScene,
     physics: {
         default: "arcade",
         arcade: {
-            debug: true,
+            debug: false,
         }
     }
 };
